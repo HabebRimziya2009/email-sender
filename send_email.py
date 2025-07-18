ae1e7c6aa667828761bb3aedbec701bc7a2dc2d4
HabebRimziya2009 <50742128+HabebRimziya2009@users.noreply.github.com>
2025-07-18 16:18:32 +1000

    Update send_email.py

--- send_email.py
+++ send_email.py
@@ -1,10 +1,12 @@
 import os
 
 from flask import Flask, request
+from flask_cors import CORS
 import smtplib
 from email.message import EmailMessage
 
 app = Flask(__name__)
+CORS(app)
 
 
 @app.route("/send", methods=["POST"])
